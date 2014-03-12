e7fa5502f30f8fbe0ff7fe1fc5f7ea6fb40f5267
Justin Richardson <richardson@hcs.ufl.edu>
2014-03-12 12:10:07 -0400

    Version:0.0.1 Description: Inial Commit for test project
     Initial commit
     Changes to be committed:
    	new file:   README.md
    
    	new file:   src/hello_world.c

---
+++ src/hello_world.c
@@ -0,0 +1,18 @@
+//=========================================================%
+//CHREC Hello World on Github                              %
+//Author: Justin Richardson                                %
+//Version: 00.00.001                                       %
+//Description: This document is a test for the             %
+//    functionality of GitHub for our applications         %
+//=========================================================%
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+
+    return 0;
+
+}
+
+//EoF
